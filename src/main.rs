7c155b6bd74322d7c661f09df1b4c2da3c75ed96
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-02 15:46:48 -0300

    restructure main loops based in comment https://github.com/sofiazcoaga/conways-game-of-life/pull/2#discussion_r2121821392

--- src/main.rs
+++ src/main.rs
@@ -11,45 +11,50 @@ async fn main() {
     let mut initial_cells_position: Vec<Position> = Vec::new();
 
     loop {
-        clear_background(WHITE);
-        if !game_on {
-            draw_text(
-                "Choose some squares and press ENTER to start",
-                50.,
-                50.,
-                30.,
-                BLACK,
-            );
-
-            for pos in &initial_cells_position {
-                draw_rectangle(pos.0, pos.1, DEFAULT_CELL_LEN, DEFAULT_CELL_LEN, BLUE);
-            }
-
-            if is_mouse_button_pressed(MouseButton::Left) {
-                let mouse_position = mouse_position();
-                let mapped_position = map_mouse_position_to_cell(mouse_position);
-
-                //Update existing grid to include selected alive cells
-                let cell = cell_grid
-                    .iter_mut()
-                    .find(|cell| cell.x() == mapped_position.0 && cell.y() == mapped_position.1);
-
-                match cell {
-                    None => println!("Selected cell is out of scope"),
-                    Some(x) => {
-                        x.set_alive(true);
-                        initial_cells_position.push(Position(mapped_position.0, mapped_position.1))
-                    }
-                }
-            } else if is_key_down(KeyCode::Enter) {
-                game_on = true;
-            }
-        } else {
-            // Include a very brief pause to see the effects more clearly (slower)
-            thread::sleep(SCREEN_DELAY_IN_MILLIS);
-            draw_grid(&cell_grid);
-            update_cells(&mut cell_grid);
+        if game_on {
+            break;
         }
+        clear_background(WHITE);
+
+        draw_text(
+            "Choose some squares and press ENTER to start",
+            50.,
+            50.,
+            30.,
+            BLACK,
+        );
+
+        for pos in &initial_cells_position {
+            draw_rectangle(pos.0, pos.1, DEFAULT_CELL_LEN, DEFAULT_CELL_LEN, BLUE);
+        }
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let mouse_position = mouse_position();
+            let mapped_position = map_mouse_position_to_cell(mouse_position);
+
+            //Update existing grid to include selected alive cells
+            let cell = cell_grid
+                .iter_mut()
+                .find(|cell| cell.x() == mapped_position.0 && cell.y() == mapped_position.1);
+
+            match cell {
+                None => println!("Selected cell is out of scope"),
+                Some(x) => {
+                    x.set_alive(true);
+                    initial_cells_position.push(Position(mapped_position.0, mapped_position.1));
+                }
+            }
+        } else if is_key_down(KeyCode::Enter) {
+            game_on = true;
+        }
+        next_frame().await;
+    }
+    loop {
+        clear_background(WHITE);
+        // Include a very brief pause to see the effects more clearly (slower)
+        thread::sleep(SCREEN_DELAY_IN_MILLIS);
+        draw_grid(&cell_grid);
+        update_cells(&mut cell_grid);
         next_frame().await;
     }
 }
